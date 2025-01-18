463133d43f38143fd53ababa643a05bc7644fa93
athenexfolk <folkadpocket99@gmail.com>
2025-01-18 23:11:14 +0700

    add pwa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,20 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 import { registerLocaleData } from '@angular/common';
 import localeTh from '@angular/common/locales/th';
+import { provideServiceWorker } from '@angular/service-worker';
 
 registerLocaleData(localeTh, 'th-TH');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
   ],
 };
